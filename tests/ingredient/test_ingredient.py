7973f42ece90a53a8dbe607425fdd5ce210393be
Rayanderson Sousa <rayandersoncooper@gmail.com>
2023-06-20 19:59:53 -0300

    tests: Implementa testes para a classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,7 +1,21 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    """Initial Commit"""
-    pass
+    ingredient = Ingredient("camarão")
+
+    assert ingredient == Ingredient("camarão")
+
+    assert ingredient.name == "camarão"
+
+    assert repr(ingredient) == f"Ingredient('camarão')"
+
+    assert hash(ingredient) == hash(Ingredient("camarão"))
+    assert hash(ingredient) != hash(Ingredient("carne"))
+
+    assert ingredient.restrictions == {
+        Restriction.ANIMAL_DERIVED,
+        Restriction.ANIMAL_MEAT,
+        Restriction.SEAFOOD,
+    }
