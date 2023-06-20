5b241fb5c098dd0f83fd04f53cd81204d685c582
Rayanderson Sousa <rayandersoncooper@gmail.com>
2023-06-20 20:20:30 -0300

    tests: Implementa testes para a classe Dish

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,27 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish, Ingredient  # noqa: F401, E261, E501
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    pudim = Dish("pudim", 5.50)
+    ingredient = Ingredient("leite")
+
+    assert pudim == Dish("pudim", 5.50)
+    assert pudim != Dish("bolo", 7.50)
+
+    assert pudim.name == "pudim"
+
+    assert hash(pudim) == hash(repr(pudim))
+    assert repr(pudim) == "Dish('pudim', R$5.50)"
+
+    pudim.add_ingredient_dependency(ingredient, 1)
+
+    assert pudim.get_ingredients() == {ingredient}
+    assert pudim.get_restrictions() == ingredient.restrictions
+
+    with pytest.raises(TypeError):
+        Dish("pudim", "5.50")
+
+    with pytest.raises(ValueError):
+        Dish("pudim", -5)
